983729b8d837cd73590e400d6a66099d3339ec90
mainfranc <74810953+mainfranc@users.noreply.github.com>
2020-11-21 11:19:09 +0300

    Update Pract2.py

--- Pract2.py
+++ Pract2.py
@@ -1,6 +1,11 @@
 from random import randint
 
 # lambda
+# Создать список, в котором каждый элемент – кортеж из двух чисел. Отсортировать данный список по убыванию вторых элементов кортежей.​
+#
+# Отсортируйте список слов по убыванию длины слова.​
+#
+# Реализуйте пример замыкания (например, «инкрементатор»)
 # 1
 lst = [(randint(0,9), randint(0,9)) for i in range(6)]
 
@@ -14,10 +19,23 @@ def sort_lst2(lst):
     return sorted(lst, key=lambda x: len(x), reverse=True)
 
 # 3
-def increment():
-    r_var = randint(0,2)
+def incrementer(start_val):
+    r_var = start_val
     def inner():
         nonlocal r_var
-        r_var += 1
-        print('print ' + r_var)
+        m = randint(2,5)
+        a = randint(0,m + 5)
+        c = randint(0,m + 5)
+        r_var = (a * r_var + c) % m
+        return r_var
     return inner
+test_func = incrementer(2)
+
+print(test_func())
+print(test_func())
+print(test_func())
+print(test_func())
+print(test_func())
+print(test_func())
+print(test_func())
+print(test_func())
