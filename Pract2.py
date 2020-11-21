f8e40de4f4cd32b519ccc1cbafcf2e3ceec05fbe
mainfranc <74810953+mainfranc@users.noreply.github.com>
2020-11-21 10:41:32 +0300

    Update Pract2.py

--- Pract2.py
+++ Pract2.py
@@ -1,1 +1,18 @@
 
+from random import randint
+# lambda
+
+# 1
+lst = [(randint(0,9), randint(0,9)) for i in range(6)]
+
+def sort_lst1(lst):
+    return sorted(lst, key=lambda x: x[1])
+
+print(sort_lst1(lst))
+# 2
+lst = ['привет', 'a', 'кто']
+def sort_lst2(lst):
+    return sorted(lst, key=lambda x: -len(x))
+
+
+print(sort_lst2(lst))
