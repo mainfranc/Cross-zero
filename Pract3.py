1a553c168c1392bf44956ba5032b7ac3777a6c4b
mainfranc <74810953+mainfranc@users.noreply.github.com>
2020-11-28 10:59:50 +0300

    Update Pract3.py

--- Pract3.py
+++ Pract3.py
@@ -1,3 +1,6 @@
+import json
+
+
 # files
 
 def file_create(f_name):
@@ -10,5 +13,18 @@ def file_create(f_name):
             print(line_)
 
 
-file_create('test.txt')
+#file_create('test.txt')
+
+
+# JSON
+def look_at_json(in_struct):
+    val = json.dumps(in_struct)
+    print(val)
+    with open('test.txt', "w") as f:
+        f.write(val)
+
+
+comp_struct = {i: [str(k) for k in range(5)] for i in range(7)}
+look_at_json(comp_struct)
+
 
