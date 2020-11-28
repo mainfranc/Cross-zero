58a870e041026f0951cdac55a128bac25a75f6e2
mainfranc <74810953+mainfranc@users.noreply.github.com>
2020-11-28 10:44:35 +0300

    Update Pract3.py

--- Pract3.py
+++ Pract3.py
@@ -1,14 +1,14 @@
-# 1
+# files
 
-def file_create(f_name, f_text):
-    f = open(f_name, "w")
-    f.write(f_text)
-    f.close()
-
-    f = open(f_name, "rb")
-    for line_ in f:
-        print(line_)
-    f.close()
+def file_create(f_name):
+    with open(f_name, "w") as f:
+        f.write(input())
 
 
-file_create('test.txt', 'какой-то текст' + '/n' + 'rgrgrgr')
+    with open(f_name, "rb") as f:
+        for line_ in f:
+            print(line_)
+
+
+file_create('test.txt')
+
