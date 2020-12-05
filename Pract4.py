0dc7bffbc8cb05a50b74acfd25ec5a3b222de544
mainfranc <74810953+mainfranc@users.noreply.github.com>
2020-12-05 11:44:35 +0300

    Update Pract4.py

--- Pract4.py
+++ Pract4.py
@@ -6,6 +6,6 @@ print(re.match(r"^((([0-9])|([1-9][0-9])|(1[0-9][0-9])|(2[0-4][0-9])|(25[0-5]))\
 nat_num = r"^[1-9]$|([0-9]{2,})$"
 abb = r"\s[A-ZА-ЯЁ]{1,}\s"
 empty_string = r"^\s*$"
-rand_text = r"(\w*(\s|\.))*"
+rand_text = r"[A-ZА-Я]([а-яa-z0-9]*\s*)*\."
 e_mail = r"([a-zA-Z0-9]){1,}\@([a-zA-Z0-9]){1,}\.(ru|com)"
 url_get = r'http(s)*:\\\\[a-zA-Z]{1,}.(ru|com)(\\[0-9a-zA-Z]*)*\?([0-9a-zA-Z]*)\=([0-9a-zA-Z]*)'
